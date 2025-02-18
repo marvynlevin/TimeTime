62ca0099fdc4076ed4db836b1c7184a43693f427
marvynlevin <marvyn.levin25460@gmail.com>
2025-02-18 09:43:02 +0100

    Initial Commit

---
+++ TimeTime/TimeTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTimeApp.swift
+//  TimeTime
+//
+//  Created by levin marvyn on 18/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct TimeTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
